678e4ad9c00deff2e73b77ad046a5ee5ccea6146
Vikram <kmarkiv@gmail.com>
2019-01-26 19:43:13 +0000

    With answer tokens

--- check_question_media.py
+++ check_question_media.py
@@ -16,15 +16,19 @@ for (name,) in cursor:
 FILE_NAMES.append("silence")
 MISSING_KEYS = []
 MISSING_QUESTION_KEYS = []
-media_query = ("SELECT question_recordings,id FROM cms_questions")
+media_query = ("SELECT question_recordings,id,trial_id FROM cms_questions")
+#media_query = ("SELECT option_text,id,question_id FROM cms_options WHERE correct=1")
+#media_query = ("SELECT soundfile_name,id,id FROM cms_tokens")
+
 cursor.execute(media_query)
-for (question_recordings, question_id) in cursor:
+for (question_recordings, question_id, trial_id) in cursor:
     # print question_recordings
+    # recording_list = [question_recordings]
     recording_list = eval(question_recordings)
     for recording in recording_list:
         r = slugify(recording, separator='_')
-        if r not in FILE_NAMES:
-            print r, recording, question_recordings, question_id
+        if (r not in FILE_NAMES) and (len(r) > 0):
+            print r, recording, question_recordings, question_id, trial_id
             MISSING_KEYS.append(r)
             MISSING_QUESTION_KEYS.append(question_id)
 
